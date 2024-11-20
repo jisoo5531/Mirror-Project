65049dc2012d51d53be52308bbf8a4157538d0fc
jisoo5531 <jisoo5531@naver.com>
2024-11-20 22:56:39 +0900

    Initial commit

---
+++ Assets/TopDownEngine/ThirdParty/MoreMountains/MMTools/Tools/MMAttributes/MMMonoBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace MoreMountains.Tools
+{
+	public class MMMonoBehaviour : NetworkBehaviour
+	{
+
+	}
+}
